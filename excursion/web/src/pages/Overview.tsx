9c3c1f7db212c952b766ae172307a1050018db63
Vegard Birkenes <vegard.birkenes@gmail.com>
2023-08-24 11:16:49 +0200

    Overview page first-draft

--- excursion/web/src/pages/Overview.tsx
+++ excursion/web/src/pages/Overview.tsx
@@ -1,39 +1,41 @@
-'use client'
+'use client';
 
 import {
   Box,
-  Center,
-  useColorModeValue,
-  Heading,
+  SimpleGrid,
   Text,
   Stack,
+  Center,
   Image,
-  Card, 
-  CardHeader, 
-  CardBody, 
-  CardFooter,
-  Button,
-  SimpleGrid
-} from '@chakra-ui/react'
+  Heading,
+  Button
+} from '@chakra-ui/react';
 
-'use client'
+interface FeatureProps {
+  title: string;
+  subtitle: string;
+  text: string;
+  url: string;
+  price: string;
+}
 
-const IMAGE =
-  'https://stexploreitera001.blob.core.windows.net/images/Rib.jpeg'
-
-export default function OverviewCard() {
+const Feature = ({ title, subtitle, text, url, price }: FeatureProps) => {
   return (
-    <Center py={12} minW={'100%'} backgroundColor={''}>
+    <Center py={12}>
       <Box
         role={'group'}
         p={6}
         maxW={'330px'}
+        borderColor={'#FFB46D'}
+        borderRadius={'1px'}
+        borderWidth={'1px'}
         w={'full'}
-        bg={useColorModeValue('#091E3B')}
+        bg={'#091E3B'}
         boxShadow={'2xl'}
         rounded={'lg'}
         pos={'relative'}
-        zIndex={1}>
+        zIndex={1}
+      >
         <Box
           rounded={'lg'}
           mt={-12}
@@ -47,42 +49,92 @@ export default function OverviewCard() {
             pos: 'absolute',
             top: 5,
             left: 0,
-            backgroundImage: `url(${IMAGE})`,
+            backgroundImage: `url(${url})`,
             filter: 'blur(15px)',
-            zIndex: -1,
+            zIndex: -1
           }}
           _groupHover={{
             _after: {
-              filter: 'blur(20px)',
-            },
-          }}>
+              filter: 'blur(20px)'
+            }
+          }}
+        >
           <Image
             rounded={'lg'}
             height={230}
             width={282}
             objectFit={'cover'}
-            src={IMAGE}
+            src={url}
             alt="#"
           />
         </Box>
         <Stack pt={10} align={'center'}>
-          <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={700} Text color={'#FFB46D'} textTransform={'uppercase'}>
-            Rib Trip
+          <Heading
+            fontSize={'2xl'}
+            fontFamily={'body'}
+            fontWeight={700}
+            color={'#FFB46D'}
+            textTransform={'uppercase'}
+          >
+            {title}
           </Heading>
-          <Text color={'#FFB46D'} fontSize={'sm'} fontWeight={700} textTransform={'uppercase'}>
-          Embark on an Arctic Adventure: 
+          <Text
+            color={'#FFB46D'}
+            fontSize={'sm'}
+            fontWeight={700}
+            textTransform={'uppercase'}
+          >
+            {subtitle}
           </Text>
-          <Text color={'white'} fontSize={'sm'} textTransform={'uppercase'}>The Thrilling Rib Boat Excursion</Text>
+          <Text color={'white'} fontSize={'sm'} textTransform={'uppercase'}>
+            {text}
+          </Text>
           <Stack direction={'row'} align={'center'}>
-            <Text fontWeight={800} fontSize={'xl'} Text color={'white'}>
-              1000NOK
+            <Text fontWeight={800} fontSize={'xl'} color={'white'}>
+              {price}
             </Text>
           </Stack>
-          <Button backgroundColor={'#FFB46D'} variant='solid'>
-           View Here
-          </Button> 
+          <Button backgroundColor={'#FFB46D'} variant="solid">
+            View Here
+          </Button>
         </Stack>
       </Box>
     </Center>
-  )
+  );
+};
+
+export default function SimpleThreeColumns() {
+  return (
+    <Box p={4} backgroundColor={'#091E3B'}>
+      <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
+        <Feature
+          url={
+            'https://stexploreitera001.blob.core.windows.net/images/Rib.jpeg'
+          }
+          title={'RIB Trip'}
+          subtitle={'Embark on an Arctic Adventure:'}
+          text={'The Thrilling RIB Boat Excursion'}
+          price={'1000 NOK'}
+        />
+        <Feature
+          url={
+            'https://stexploreitera001.blob.core.windows.net/images/Dog%20Sled.jpeg'
+          }
+          title={'Dog Sled'}
+          subtitle="Experience the Arctic Magic:"
+          text={'Dog Sled Excursion'}
+          price={'1250 NOK'}
+        />
+        <Feature
+          url={
+            'https://stexploreitera001.blob.core.windows.net/images/Hvalsafari.png'
+          }
+          title={'Whale Safari'}
+          subtitle="Experience the Arctic Magic:"
+          text={'A Majestic Adventure'}
+          price={'800'}
+        />
+      </SimpleGrid>
+    </Box>
+  );
 }
