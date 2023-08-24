0ded003071b5c3b9e83d139153db1fbac22b4098
Hjalti <hjaltipeh@gmail.com>
2023-08-24 10:38:45 +0200

    added persona
    Persona.png added

--- spa/web/src/Navbar.tsx
+++ spa/web/src/Navbar.tsx
@@ -1,6 +1,8 @@
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
 import ExplorerLogo from "./assets/ExploreLogo.svg"
+import Persona from "./assets/Persona.png"
+
 function Navbar() {
     return(
         <nav className="Navbar-container">
@@ -8,6 +10,7 @@ function Navbar() {
             <NavLink  to="/" className="Navbar-title">
                 SPA
             </NavLink>
+            <NavLink to="/"><img src={Persona} alt="Profile Picture" width={50}/></NavLink>
         </nav>
     )
 }
